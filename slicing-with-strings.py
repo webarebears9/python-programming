b669073be2528a67f13ab55ca01bc87f35577d59
webarebears9 <tulika.bhatia.3@gmail.com>
2026-04-03 18:44:45 +0530

    Added slicing-with-strings.py

---
+++ slicing-with-strings.py
@@ -0,0 +1,15 @@
+my_string = "Hello World"
+print(my_string[0])
+print(my_string[-2])
+print(my_string[-3])
+my_string="abcdefghi"
+print(my_string)
+print(my_string[2:])
+print(my_string[:3])
+print(my_string)
+print(my_string[3:6])
+print(my_string[1:3])
+print(my_string[::])
+print(my_string[::3])
+print(my_string[2:7:2])
+print(my_string[::-1])
